04e87b594c3c241520eaf2c9410a44c4860664c4
Zwiwi <129974715+Zwiwi@users.noreply.github.com>
2023-04-05 17:33:54 +0200

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit 
 
-streamlit.title('Breakfast favorites')
+streamlit.title('My Moms new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
